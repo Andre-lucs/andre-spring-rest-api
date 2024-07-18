ea7e1c1f18a0ce0526525c2419cd8b032885588f
Andre-lucs <andreltd312@gmail.com>
2024-07-17 23:56:53 -0300

    feat : added pagination on Book requests

--- src/main/java/org/andrelucs/examplerestapi/controller/BookController.java
+++ src/main/java/org/andrelucs/examplerestapi/controller/BookController.java
@@ -9,6 +9,11 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.andrelucs.examplerestapi.model.dto.BookDTO;
 import org.andrelucs.examplerestapi.service.BookService;
 import org.andrelucs.examplerestapi.util.MediaType;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -40,8 +45,16 @@ public class BookController {
             @ApiResponse(responseCode = "404", description = "Not Found", content = @Content),
             @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content)
         })
-    public List<BookDTO> findAll(){
-        return (bookService.findAll());
+    public PagedModel<EntityModel<BookDTO>> findAll(
+            @RequestParam(defaultValue = "0") Integer page,
+            @RequestParam(defaultValue = "20") Integer size,
+            @RequestParam(defaultValue = "asc") String direction
+    ){
+        var sorting = direction.equalsIgnoreCase("desc") ? Sort.Direction.DESC : Sort.Direction.ASC;
+
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sorting, "title"));
+
+        return bookService.findAll(pageable);
     }
 
     @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
--- src/main/java/org/andrelucs/examplerestapi/service/BookService.java
+++ src/main/java/org/andrelucs/examplerestapi/service/BookService.java
@@ -1,10 +1,19 @@
 package org.andrelucs.examplerestapi.service;
 
 import org.andrelucs.examplerestapi.controller.BookController;
+import org.andrelucs.examplerestapi.exceptions.BadRequestException;
+import org.andrelucs.examplerestapi.model.Book;
 import org.andrelucs.examplerestapi.model.dto.BookDTO;
 import org.andrelucs.examplerestapi.exceptions.NotFoundException;
+import org.andrelucs.examplerestapi.model.dto.PersonDTO;
 import org.andrelucs.examplerestapi.model.mapping.BookMapper;
 import org.andrelucs.examplerestapi.repository.BookRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,18 +26,26 @@ public class BookService {
     private final BookRepository bookRepository;
 
     private final BookMapper mapper;
+    private final PagedResourcesAssembler<BookDTO> pagedResourcesAssembler;
 
-    public BookService(BookRepository bookRepository, BookMapper bookMapper){
+
+    public BookService(BookRepository bookRepository, BookMapper mapper, PagedResourcesAssembler<BookDTO> pagedResourcesAssembler) {
         this.bookRepository = bookRepository;
-        this.mapper = bookMapper;
+        this.mapper = mapper;
+        this.pagedResourcesAssembler = pagedResourcesAssembler;
     }
 
-    public List<BookDTO> findAll(){
-        var books = bookRepository.findAll();
-        return mapper.bookListToBookDTOList(books).stream()
-                .map(book -> book.add(
-                        linkTo(methodOn(BookController.class).findById(book.getKey())).withSelfRel())
-                ).toList();
+    public PagedModel<EntityModel<BookDTO>> findAll(Pageable pageable){
+        Page<Book> books = bookRepository.findAll(pageable);
+
+        return getEntityModels(pageable, books);
+    }
+
+    private PagedModel<EntityModel<BookDTO>> getEntityModels(Pageable pageable, Page<Book> books) {
+        Page<BookDTO> booksDTOs = books.map(mapper::bookToBookDTO);
+        booksDTOs.map(bookDTO -> linkTo(methodOn(BookController.class).findById(bookDTO.getKey())).withSelfRel());
+        Link allLink = linkTo(methodOn(BookController.class).findAll(pageable.getPageNumber(), pageable.getPageSize(), "asc")).withSelfRel();
+        return pagedResourcesAssembler.toModel(booksDTOs, allLink);
     }
 
     public BookDTO findById(Long id){
@@ -45,6 +62,8 @@ public class BookService {
     }
 
     public BookDTO update(BookDTO bookDTO){
+        if(bookDTO == null || bookDTO.getKey() == null)
+            throw new BadRequestException("Invalid book data");
         var oldBook = bookRepository.findById(bookDTO.getKey());
         if(oldBook.isEmpty()){
             throw new NotFoundException("Book not found");
